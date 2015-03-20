57f4619c3671c5368e01afe9a599b9e3745c8a81
Tobias Gu <t4gu@uwaterloo.ca>
2015-03-20 01:03:11 -0400

    Clear Logs button added

--- app/src/main/java/com/example/saibot/test1/LogsActivity.java
+++ app/src/main/java/com/example/saibot/test1/LogsActivity.java
@@ -71,6 +71,76 @@ public class LogsActivity extends Activity {
         toggleMenu();
     }
 
+    public void ClearLogs(View view) {
+        new clearLogs().execute();
+        new getLogs().execute();
+        Log.w("Debug", "Getting logs");
+        TextView t;
+        t = (TextView) findViewById(R.id.logView);
+        t.setText("Loading New Logs... Please wait");
+    }
+    private class clearLogs extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object... arg0) {
+            try {
+                cleerLogs();
+                return null;
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+    private void cleerLogs() throws Exception {
+
+        String device = "lock";
+        String action = "logclear";
+        String id = "ANDROID";
+
+        URL ourServer = new URL("https://warm-sierra-7577.herokuapp.com/" +
+                device + "?id=" + id + "&" + action + "=1");
+
+        Log.d("mymsg", "https://warm-sierra-7577.herokuapp.com/" +
+                device + "?id=" + id + "&" + action + "=1");
+
+        URLConnection yc = ourServer.openConnection();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(yc.getInputStream()));
+        Log.d("mymsg", "~~~~~~~~~~~~~~~~~~Authorized Buttom Pressed ~~~~~~~~~~~~~~~~~~~~");
+//      THIS IS FOR VERTICAL READING
+//      int inputLine;
+        String inputLine;
+        final StringBuffer sb = new StringBuffer("Result:");
+        Log.d("mymsg", "derp");
+
+        while ((inputLine = in.readLine()) != null)
+            sb.append(inputLine + "\n");
+        in.close();
+// THIS FOR VERTICAL READING
+/*        while ((inputLine = in.read()) != -1) {
+            if (inputLine = " ") {
+                sb.append((char) inputLine + "\n");
+            } else {
+                sb.append((char) inputLine);
+            }
+        }
+        in.close();
+*/
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                TextView t;
+
+                t = (TextView) findViewById(R.id.tv);
+                t.setText(sb);
+            }
+        });
+    }
+
     public void menu_clicked(View view) {
         Log.d("Home", "Menu button clicked from dashboard");
 
